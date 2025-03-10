dc88de21411f9d230b116fb030a2a0ec3733b384
Luca Severo <luca98.severo@gmail.com>
2025-03-10 06:33:45 -0300

    Flat model created

---
+++ db/migrate/20250310093305_create_flats.rb
@@ -0,0 +1,10 @@
+class CreateFlats < ActiveRecord::Migration[7.1]
+  def change
+    create_table :flats do |t|
+      t.string :address
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
